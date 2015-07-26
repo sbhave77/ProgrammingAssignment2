3b2c50dea361f13b72d454f2b948f1273cc71c29
Shreyas Bhave <sbhave@berkeley.edu>
2015-07-26 15:06:47 -0700

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
 ## Put comments here that give an overall description of what your
 ## functions do
+# Both these functions used in conjunction with each other allow for storage of the inverse
+# of a matrix so as to prevent recaclulating over and over again. 
 
 ## Write a short comment describing this function
+## makeCacheMatrix is analogous to a class in an object oriented programming language like java.
+# It takes in a matrix and stores that matrix in a setMatrix attribute which is also a function.
+# It also sets the attribute inverse to null and stores the inverse when setInverse is called.
+# It returns a list of functions with stored variables.
 
 makeCacheMatrix <- function(x = matrix()) {
       inverse <- NULL
@@ -19,6 +25,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+# cacheSolve takes in a special cacheMatrix "object" (list) that was made using the previous function
+# It gets the inverse. if the inverse is already stored, then it uses that inverse, otherwise it
+# calculates the inverse and sets the inverse using the function setInverse.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
